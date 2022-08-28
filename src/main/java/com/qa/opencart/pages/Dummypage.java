8000ae3336afe1e000d0be2b693f2a2e875fc717
ragivrehmani <ragiv.rehmani@paloalosoft.com>
2022-08-28 21:56:35 +0530

    this is my dummy code

---
+++ src/main/java/com/qa/opencart/pages/Dummypage.java
@@ -0,0 +1,12 @@
+package com.qa.opencart.pages;
+
+public class Dummypage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("dummy page");
+		
+	}
+
+}
